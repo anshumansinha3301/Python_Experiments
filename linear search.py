d677744b92ff08a350c3ee9b62a4a3c8f4198a26
Anshuman Sinha <138311612+anshumansinha3301@users.noreply.github.com>
2024-03-03 17:07:55 +0530

    Add files via upload

---
+++ linear search.py
@@ -0,0 +1,15 @@
+def linear_search(arr, target):
+    for i, value in enumerate(arr):
+        if value == target:
+            return i 
+    return -1 
+
+my_list = [4, 2, 8, 1, 7, 5, 9]
+target_value = 7
+
+result = linear_search(my_list, target_value)
+
+if result != -1:
+    print(f'Target value {target_value} found at index {result}.')
+else:
+    print(f'Target value {target_value} not found in the list.')
